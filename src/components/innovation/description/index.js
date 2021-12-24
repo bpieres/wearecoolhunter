287af6e69789229babfe541ced69f62dc8f801be
bpieres <benjapieres@gmail.com>
2021-12-24 10:20:57 -0300

    correccion de punto

--- src/components/innovation/description/index.js
+++ src/components/innovation/description/index.js
@@ -5,7 +5,7 @@ const InnovationDescription = () => {
     return (
         <div className="innovation__description">
             <h2 className="is-size-3 is-size-2-desktop py-3 has-text-white has-text-centered">
-                We challenge business <b>transformation</b> .
+                We challenge business <b>transformation</b>.
             </h2>
 
             <ArrowDownIcon
